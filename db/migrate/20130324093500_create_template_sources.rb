e865d2f6213987c1b94704c326758b36ab69dcd7
sleeping-lion <toughjjh@gmail.com>
2013-12-27 21:08:08 +0900

    gg

--- db/migrate/20130324093500_create_template_sources.rb
+++ db/migrate/20130324093500_create_template_sources.rb
@@ -1,7 +1,7 @@
 class CreateTemplateSources < ActiveRecord::Migration
   def change
     create_table :template_sources do |t|
-      t.string :name, :null=>false, :limit=>60
+      t.string :name, :null=>false
       t.string :korean_name, :limit=>60      
       t.boolean :enable, :null=>false, :default=>true
       t.timestamps
